1e6507735523e0785a846fd35280e4e1d0384a6c
alazif81 <auth.co.in+github@gmail.com>
2025-10-08 09:23:31 +0900

    Add a new Japanese keyboard layout.

--- ui/src/keyboardLayouts.ts
+++ ui/src/keyboardLayouts.ts
@@ -25,7 +25,8 @@ import { fr_BE } from "@/keyboardLayouts/fr_BE"
 import { fr_CH } from "@/keyboardLayouts/fr_CH"
 import { fr_FR } from "@/keyboardLayouts/fr_FR"
 import { it_IT } from "@/keyboardLayouts/it_IT"
+import { ja_JP } from "@/keyboardLayouts/ja_JP"
 import { nb_NO } from "@/keyboardLayouts/nb_NO"
 import { sv_SE } from "@/keyboardLayouts/sv_SE"
 
-export const keyboards: KeyboardLayout[] = [ cs_CZ, de_CH, de_DE, en_UK, en_US, es_ES, fr_BE, fr_CH, fr_FR, it_IT, nb_NO, sv_SE ];
+export const keyboards: KeyboardLayout[] = [ cs_CZ, de_CH, de_DE, en_UK, en_US, es_ES, fr_BE, fr_CH, fr_FR, it_IT, ja_JP, nb_NO, sv_SE ];
